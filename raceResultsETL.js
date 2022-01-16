177c46e0704e59b5af66532f64ac7903019f9db2
Cory Ellerbroek <cory.ellerbroek@gmail.com>
2022-01-15 21:32:35 -0600

    Successful parse of csv. writeToStore in progress.

--- raceResultsETL.js
+++ raceResultsETL.js
@@ -1,37 +1,41 @@
 import { db } from './src/firebase.ts';
-import { writeBatch } from 'firebase/firestore';
+import { writeBatch, batch } from 'firebase/firestore';
 import { readFile } from 'fs/promises';
 import { promisify } from 'util';
 import { parse } from 'csv-parse';
-// console.log(csvParse);
-// const parse = promisify(csvParse);
+const parser = promisify(parse);
 
 if (process.argv.length < 3) {
   console.error('Please include a path to a csv file');
   process.exit(1);
 }
 
-function writeToFirestore(records) {
-  const batchCommits = [];
-  let batch = db.batch();
+async function writeToFirestore(records, db) {
+  //   const batchCommits = [];
+  // const batch = db.batch();
   records.forEach((record, i) => {
-    var docRef = db.collection('rainfall').doc(record.SUBDIVISION);
+    var docRef = db
+      .collection('events')
+      .doc('dragonslayer-2021-11-21')
+      .collection('results')
+      .doc();
     batch.set(docRef, record);
-    if ((i + 1) % 500 === 0) {
-      console.log(`Writing record ${i + 1}`);
-      batchCommits.push(batch.commit());
-      batch = db.batch();
-    }
+    // if ((i + 1) % 500 === 0) {
+    //   console.log(`Writing record ${i + 1}`);
+    //   batchCommits.push(batch.commit());
+    //   batch = db.batch();
+    // }
   });
-  batchCommits.push(batch.commit());
-  return Promise.all(batchCommits);
+  //   batchCommits.push(batch.commit());
+  //   return Promise.all(batchCommits);
+  await batch.commit();
 }
 
 async function importCsv(csvFileName) {
   const fileContents = await readFile(csvFileName, 'utf8');
-  const records = await parse(fileContents, { columns: true });
+  const records = await parser(fileContents, { columns: true });
   try {
-    await writeToFirestore(records);
+    await writeToFirestore(records, db);
   } catch (e) {
     console.error(e);
     process.exit(1);
